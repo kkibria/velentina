9db14b11d8424bef69e885ec706845da32ae59c2
dismine <dismine@gmail.com>
2013-12-18 13:13:32 +0200

    New tool CutSpline (Drawing mode).

---
+++ src/widgets/vsimplespline.cpp
@@ -0,0 +1,59 @@
+/************************************************************************
+ **
+ **  @file   vsimplespline.cpp
+ **  @author Roman Telezhinsky <dismine@gmail.com>
+ **  @date   17 12, 2013
+ **
+ **  @brief
+ **  @copyright
+ **  This source code is part of the Valentine project, a pattern making
+ **  program, whose allow create and modeling patterns of clothing.
+ **  Copyright (C) 2013 Valentina project
+ **  <https://bitbucket.org/dismine/valentina> All Rights Reserved.
+ **
+ **  Valentina is free software: you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation, either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  Valentina is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with Valentina.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ *************************************************************************/
+
+#include "vsimplespline.h"
+#include "../options.h"
+
+VSimpleSpline::VSimpleSpline(qint64 id, qreal *factor, Qt::GlobalColor *currentColor, QObject *parent)
+    :QObject(parent), QGraphicsPathItem(), id (id), factor(factor), currentColor(currentColor)
+{
+    setPen(QPen(Qt::black, widthHairLine/ *factor));
+    setFlag(QGraphicsItem::ItemIsSelectable, true);
+    setAcceptHoverEvents(true);
+}
+
+void VSimpleSpline::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        emit Choosed(id);
+    }
+    QGraphicsItem::mouseReleaseEvent(event);
+}
+
+void VSimpleSpline::hoverMoveEvent(QGraphicsSceneHoverEvent *event)
+{
+    Q_UNUSED(event);
+    this->setPen(QPen(*currentColor, widthMainLine/ *factor));
+}
+
+void VSimpleSpline::hoverLeaveEvent(QGraphicsSceneHoverEvent *event)
+{
+    Q_UNUSED(event);
+    this->setPen(QPen(*currentColor, widthHairLine/ *factor));
+}
