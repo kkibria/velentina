289dfda4b11637c7ee8bdee25a38e433c4d74404
Roman Telezhynskyi <kroluku@gmail.com>
2016-07-30 15:45:04 +0300

    Merged in BojanKverh/valentina-bojan/feature (pull request #128)
    Resolved issue #24. New feature: Label on detail.

--- src/test/ValentinaTest/abstracttest.cpp
+++ src/test/ValentinaTest/abstracttest.cpp
@@ -176,9 +176,20 @@ bool AbstractTest::CopyRecursively(const QString &srcFilePath, const QString &tg
             }
         }
 
-        if (not QFile::copy(srcFilePath, tgtFilePath))
+        // Check error: Cannot open %file for input
+        QFile srcFile(srcFilePath);
+        if (not srcFile.open(QFile::ReadOnly))
         {
-            const QString msg = QString("Can't copy file '%1' to '%2'.").arg(srcFilePath).arg(tgtFilePath);
+            const QString msg = QString("Can't copy file '%1'. Error: %2").arg(srcFilePath).arg(srcFile.errorString());
+            QWARN(qUtf8Printable(msg));
+            return false;
+        }
+        srcFile.close();
+
+        if (not srcFile.copy(tgtFilePath))
+        {
+            const QString msg = QString("Can't copy file '%1' to '%2'. Error: %3").arg(srcFilePath).arg(tgtFilePath)
+                    .arg(srcFile.errorString());
             QWARN(qUtf8Printable(msg));
             return false;
         }
