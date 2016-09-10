8ec669824c4423ca372278059ec0088412253d76
Roman Telezhynskyi <kroluku@gmail.com>
2016-09-10 16:40:38 +0300

    Flipping VPointF.

---
+++ src/app/valentina/mainwindowsnogui.h
@@ -0,0 +1,143 @@
+/************************************************************************
+ **
+ **  @file   mainwindowsnogui.h
+ **  @author Roman Telezhynskyi <dismine(at)gmail.com>
+ **  @date   12 5, 2015
+ **
+ **  @brief
+ **  @copyright
+ **  This source code is part of the Valentine project, a pattern making
+ **  program, whose allow create and modeling patterns of clothing.
+ **  Copyright (C) 2015 Valentina project
+ **  <https://bitbucket.org/dismine/valentina> All Rights Reserved.
+ **
+ **  Valentina is free software: you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation, either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  Valentina is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with Valentina.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ *************************************************************************/
+
+#ifndef MAINWINDOWSNOGUI_H
+#define MAINWINDOWSNOGUI_H
+
+#include <QMainWindow>
+#include <QPrinter>
+
+#include "../vpatterndb/vdetail.h"
+#include "../vlayout/vlayoutdetail.h"
+#include "xml/vpattern.h"
+#include "dialogs/dialogsavelayout.h"
+#include "../vlayout/vlayoutgenerator.h"
+#include "../vwidgets/vabstractmainwindow.h"
+
+
+class QGraphicsScene;
+class QPrinter;
+class PosterData;
+
+class MainWindowsNoGUI : public VAbstractMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindowsNoGUI(QWidget *parent = nullptr);
+    virtual ~MainWindowsNoGUI() Q_DECL_OVERRIDE;
+
+public slots:
+    void ToolLayoutSettings(bool checked);
+    void SaveAsTiledPDF();
+    void PrintPreviewOrigin();
+    void PrintPreviewTiled();
+    void PrintOrigin();
+    void PrintTiled();
+protected:
+    QVector<VLayoutDetail> listDetails;
+
+    /** @brief currentScene pointer to current scene. */
+    QGraphicsScene *currentScene;
+
+    QGraphicsScene *tempSceneLayout;
+
+    /** @brief pattern container with data (points, arcs, splines, spline paths, variables) */
+    VContainer         *pattern;
+
+    /** @brief doc dom document container */
+    VPattern           *doc;
+
+    QList<QGraphicsItem *> papers;
+    QList<QGraphicsItem *> shadows;
+    QList<QGraphicsScene *> scenes;
+    QList<QList<QGraphicsItem *> > details;
+
+    QAction *undoAction;
+    QAction *redoAction;
+    QAction *actionDockWidgetToolOptions;
+    QAction *actionDockWidgetGroups;
+
+    /** @brief fileName name current pattern file. */
+    QString            curFile;
+
+    bool isLayoutStale;
+    QMarginsF margins;
+    QSizeF paperSize;
+
+    void PrepareDetailsForLayout(const QHash<quint32, VDetail> *details);
+    void ExportLayout(const DialogSaveLayout &dialog);
+
+    void InitTempLayoutScene();
+    virtual void CleanLayout()=0;
+    virtual void PrepareSceneList()=0;
+    QIcon ScenePreview(int i) const;
+    bool LayoutSettings(VLayoutGenerator& lGenerator);
+    int ContinueIfLayoutStale();
+    QString FileName() const;
+private slots:
+    void PrintPages (QPrinter *printer);
+    void ErrorConsoleMode(const LayoutErrors &state);
+private:
+    Q_DISABLE_COPY(MainWindowsNoGUI)
+
+    bool isTiled;
+    bool isAutoCrop;
+    bool isUnitePages;
+
+    QString layoutPrinterName;
+
+    void CreateShadows();
+    void CreateScenes();
+
+    void SvgFile(const QString &name, int i)const;
+    void PngFile(const QString &name, int i)const;
+    void PdfFile(const QString &name, int i)const;
+    void EpsFile(const QString &name, int i)const;
+    void PsFile(const QString &name, int i)const;
+    void PdfToPs(const QStringList &params)const;
+    void ObjFile(const QString &name, int i)const;
+    void DxfFile(const QString &name, int i)const;
+
+    void PreparePaper(int index) const;
+    void RestorePaper(int index) const;
+
+    void SaveLayoutAs();
+    void PrintPreview();
+    void LayoutPrint();
+
+    enum class PrintType : char {PrintPDF, PrintPreview, PrintNative};
+
+    void SetPrinterSettings(QPrinter *printer, const PrintType &printType);
+    bool IsLayoutGrayscale() const;
+    QPrinter::PaperSize FindTemplate(const QSizeF &size) const;
+
+    bool isPagesUniform() const;
+    bool IsPagesFit(const QSizeF &printPaper) const;
+};
+
+#endif // MAINWINDOWSNOGUI_H
---
+++ src/libs/vtools/visualization/visualization.pri
@@ -0,0 +1,70 @@
+# ADD TO EACH PATH $$PWD VARIABLE!!!!!!
+# This need for corect working file translations.pro
+
+HEADERS += \
+    $$PWD/visualization.h \
+    $$PWD/line/visline.h \
+    $$PWD/line/vistoolline.h \
+    $$PWD/line/vistoolendline.h \
+    $$PWD/line/vistoolalongline.h \
+    $$PWD/line/vistoolbisector.h \
+    $$PWD/line/vistoolshoulderpoint.h \
+    $$PWD/line/vistoolnormal.h \
+    $$PWD/line/vistoolheight.h \
+    $$PWD/line/vistoolpointofintersection.h \
+    $$PWD/line/vistooltriangle.h \
+    $$PWD/line/vistoolpointofcontact.h \
+    $$PWD/line/vistoollineintersect.h \
+    $$PWD/line/vistoollineintersectaxis.h \
+    $$PWD/line/vistooltruedarts.h \
+    $$PWD/line/vistoolcurveintersectaxis.h \
+    $$PWD/line/vistoolpointofintersectionarcs.h \
+    $$PWD/line/vistoolpointofintersectioncircles.h \
+    $$PWD/line/vistoolpointfromcircleandtangent.h \
+    $$PWD/line/vistoolpointfromarcandtangent.h \
+    $$PWD/path/vispath.h \
+    $$PWD/path/vistoolarc.h \
+    $$PWD/path/vistoolcutarc.h \
+    $$PWD/path/vistoolspline.h \
+    $$PWD/path/vistoolcutspline.h \
+    $$PWD/path/vistoolsplinepath.h \
+    $$PWD/path/vistoolcutsplinepath.h \
+    $$PWD/path/vistoolarcwithlength.h \
+    $$PWD/path/vistoolpointofintersectioncurves.h \
+    $$PWD/path/vistoolcubicbezier.h \
+    $$PWD/path/vistoolcubicbezierpath.h \
+    visualization/line/vistoolrotation.h
+
+SOURCES += \
+    $$PWD/visualization.cpp \
+    $$PWD/line/visline.cpp \
+    $$PWD/line/vistoolline.cpp \
+    $$PWD/line/vistoolendline.cpp \
+    $$PWD/line/vistoolalongline.cpp \
+    $$PWD/line/vistoolbisector.cpp \
+    $$PWD/line/vistoolshoulderpoint.cpp \
+    $$PWD/line/vistoolnormal.cpp \
+    $$PWD/line/vistoolheight.cpp \
+    $$PWD/line/vistoolpointofintersection.cpp \
+    $$PWD/line/vistooltriangle.cpp \
+    $$PWD/line/vistoolpointofcontact.cpp \
+    $$PWD/line/vistoollineintersect.cpp \
+    $$PWD/line/vistoollineintersectaxis.cpp \
+    $$PWD/line/vistooltruedarts.cpp \
+    $$PWD/line/vistoolcurveintersectaxis.cpp \
+    $$PWD/line/vistoolpointofintersectionarcs.cpp \
+    $$PWD/line/vistoolpointofintersectioncircles.cpp \
+    $$PWD/line/vistoolpointfromcircleandtangent.cpp \
+    $$PWD/line/vistoolpointfromarcandtangent.cpp \
+    $$PWD/path/vispath.cpp \
+    $$PWD/path/vistoolarc.cpp \
+    $$PWD/path/vistoolcutarc.cpp \
+    $$PWD/path/vistoolspline.cpp \
+    $$PWD/path/vistoolcutspline.cpp \
+    $$PWD/path/vistoolsplinepath.cpp \
+    $$PWD/path/vistoolcutsplinepath.cpp \
+    $$PWD/path/vistoolarcwithlength.cpp \
+    $$PWD/path/vistoolpointofintersectioncurves.cpp \
+    $$PWD/path/vistoolcubicbezier.cpp \
+    $$PWD/path/vistoolcubicbezierpath.cpp \
+    visualization/line/vistoolrotation.cpp
