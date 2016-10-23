9ca01eef3e4b7b3d7e3905fa3c3da8345cae121e
Ronan Le Tiec <ronanletiec@gmail.com>
2016-10-23 11:11:00 +0200

    issue #570 tiled pdf case when margin smaller than min

---
+++ src/libs/vmisc/vabstractapplication.h
@@ -0,0 +1,164 @@
+/************************************************************************
+ **
+ **  @file   vabstractapplication.h
+ **  @author Roman Telezhynskyi <dismine(at)gmail.com>
+ **  @date   18 6, 2015
+ **
+ **  @brief
+ **  @copyright
+ **  This source code is part of the Valentine project, a pattern making
+ **  program, whose allow create and modeling patterns of clothing.
+ **  Copyright (C) 2015 Valentina project
+ **  <https://bitbucket.org/dismine/valentina> All Rights Reserved.
+ **
+ **  Valentina is free software: you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation, either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  Valentina is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with Valentina.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ *************************************************************************/
+
+#ifndef VABSTRACTAPPLICATION_H
+#define VABSTRACTAPPLICATION_H
+
+#include <qcompilerdetection.h>
+#include <QApplication>
+#include <QCoreApplication>
+#include <QGraphicsScene>
+#include <QLocale>
+#include <QMetaObject>
+#include <QObject>
+#include <QPointer>
+#include <QString>
+#include <QtGlobal>
+
+#include "../vmisc/def.h"
+#include "../vpatterndb/vtranslatevars.h"
+#include "def.h"
+#include "vcommonsettings.h"
+#include "vlockguard.h"
+#include "vsettings.h"
+
+class QGraphicsScene;
+class QTranslator;
+class QUndoStack;
+class QWidget;
+class VAbstractApplication;// use in define
+class VAbstractPattern;
+class VMainGraphicsView;
+class VTranslateVars;
+
+#if defined(qApp)
+#undef qApp
+#endif
+#define qApp (static_cast<VAbstractApplication*>(QCoreApplication::instance()))
+
+class VAbstractApplication : public QApplication
+{
+    Q_OBJECT
+public:
+    VAbstractApplication(int &argc, char ** argv);
+    virtual ~VAbstractApplication() Q_DECL_OVERRIDE;
+
+    virtual const VTranslateVars *TrVars()=0;
+
+    QString          translationsPath(const QString &locale = QString()) const;
+
+    void             LoadTranslation(const QString &locale);
+
+    Unit             patternUnit() const;
+    const Unit      *patternUnitP() const;
+    void             setPatternUnit(const Unit &patternUnit);
+
+    MeasurementsType patternType() const;
+    void             setPatternType(const MeasurementsType &patternType);
+
+    virtual void     OpenSettings()=0;
+    VCommonSettings *Settings();
+
+    template <typename T>
+    QString          LocaleToString(const T &value);
+
+    QGraphicsScene  *getCurrentScene() const;
+    void             setCurrentScene(QGraphicsScene *value);
+
+    VMainGraphicsView *getSceneView() const;
+    void               setSceneView(VMainGraphicsView *value);
+
+    double           toPixel(double val) const;
+    double           fromPixel(double pix) const;
+
+    void             setCurrentDocument(VAbstractPattern *doc);
+    VAbstractPattern *getCurrentDocument()const;
+
+    bool             getOpeningPattern() const;
+    void             setOpeningPattern();
+
+    QWidget         *getMainWindow() const;
+    void             setMainWindow(QWidget *value);
+
+    QUndoStack      *getUndoStack() const;
+
+    virtual bool     IsAppInGUIMode()const =0;
+
+protected:
+    QUndoStack         *undoStack;
+
+    /**
+     * @brief mainWindow pointer to main window. Usefull if need create modal dialog. Without pointer to main window
+     * modality doesn't work.
+     */
+    QWidget            *mainWindow;
+
+    /**
+     * @brief settings pointer to settings. Help hide constructor creation settings. Make make code more readable.
+     */
+    VCommonSettings    *settings;
+
+    QPointer<QTranslator> qtTranslator;
+    QPointer<QTranslator> qtxmlTranslator;
+    QPointer<QTranslator> qtBaseTranslator;
+    QPointer<QTranslator> appTranslator;
+    QPointer<QTranslator> pmsTranslator;
+
+    virtual void InitTrVars()=0;
+
+private:
+    Q_DISABLE_COPY(VAbstractApplication)
+    Unit               _patternUnit;
+    MeasurementsType   _patternType;
+
+
+    QGraphicsScene     *currentScene;
+    VMainGraphicsView  *sceneView;
+
+    VAbstractPattern   *doc;
+
+    /**
+     * @brief openingPattern true when we opening pattern. If something will be wrong in formula this help understand if
+     * we can allow user use Undo option.
+     */
+    bool               openingPattern;
+
+    void ClearTranslation();
+};
+
+
+//---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+inline QString VAbstractApplication::LocaleToString(const T &value)
+{
+    QLocale loc;
+    qApp->Settings()->GetOsSeparator() ? loc = QLocale::system() : loc = QLocale::c();
+    return loc.toString(value);
+}
+
+#endif // VABSTRACTAPPLICATION_H
---
+++ src/libs/vtools/tools/nodeDetails/vnodearc.cpp
@@ -0,0 +1,157 @@
+/************************************************************************
+ **
+ **  @file   vnodearc.cpp
+ **  @author Roman Telezhynskyi <dismine(at)gmail.com>
+ **  @date   November 15, 2013
+ **
+ **  @brief
+ **  @copyright
+ **  This source code is part of the Valentine project, a pattern making
+ **  program, whose allow create and modeling patterns of clothing.
+ **  Copyright (C) 2013-2015 Valentina project
+ **  <https://bitbucket.org/dismine/valentina> All Rights Reserved.
+ **
+ **  Valentina is free software: you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation, either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  Valentina is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with Valentina.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ *************************************************************************/
+
+#include "vnodearc.h"
+
+#include <QDomElement>
+#include <QStaticStringData>
+#include <QStringData>
+#include <QStringDataPtr>
+
+#include "../ifc/xml/vdomdocument.h"
+#include "../ifc/ifcdef.h"
+#include "../vabstracttool.h"
+#include "../vdatatool.h"
+#include "vabstractnode.h"
+
+class VContainer;
+
+const QString VNodeArc::ToolType = QStringLiteral("modeling");
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+ * @brief VNodeArc constructor.
+ * @param doc dom document container.
+ * @param data container with variables.
+ * @param id object id in container.
+ * @param idArc object id in containerArc.
+ * @param typeCreation way we create this tool.
+ * @param idTool tool id.
+ * @param qoParent QObject parent
+ */
+VNodeArc::VNodeArc(VAbstractPattern *doc, VContainer *data, quint32 id, quint32 idArc, const Source &typeCreation,
+                   const QString &drawName, const quint32 &idTool, QObject *qoParent)
+    :VAbstractNode(doc, data, id, idArc, drawName, idTool, qoParent)
+{
+    ToolCreation(typeCreation);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+ * @brief Create help create tool.
+ * @param doc dom document container.
+ * @param data container with variables.
+ * @param id object id in container.
+ * @param idArc object id in containerArc.
+ * @param parse parser file mode.
+ * @param typeCreation way we create this tool.
+ * @param idTool tool id.
+ */
+void VNodeArc::Create(VAbstractPattern *doc, VContainer *data, quint32 id, quint32 idArc,
+                      const Document &parse, const Source &typeCreation, const QString &drawName, const quint32 &idTool)
+{
+    VAbstractTool::AddRecord(id, Tool::NodeArc, doc);
+    if (parse == Document::FullParse)
+    {
+        VNodeArc *arc = new VNodeArc(doc, data, id, idArc, typeCreation, drawName, idTool, doc);
+
+        doc->AddTool(id, arc);
+        if (idTool != NULL_ID)
+        {
+            //Some nodes we don't show on scene. Tool that create this nodes must free memory.
+            VDataTool *tool = doc->getTool(idTool);
+            SCASSERT(tool != nullptr);
+            arc->setParent(tool);// Adopted by a tool
+        }
+        else
+        {
+            // Help to delete the node before each FullParse
+            doc->AddToolOnRemove(arc);
+        }
+    }
+    else
+    {
+        doc->UpdateToolData(id, data);
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+QString VNodeArc::getTagName() const
+{
+    return VAbstractPattern::TagArc;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void VNodeArc::AllowHover(bool enabled)
+{
+    Q_UNUSED(enabled)
+    // do nothing
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void VNodeArc::AllowSelecting(bool enabled)
+{
+    Q_UNUSED(enabled)
+    // do nothing
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+ * @brief AddToFile add tag with informations about tool into file.
+ */
+void VNodeArc::AddToFile()
+{
+    QDomElement domElement = doc->createElement(getTagName());
+
+    doc->SetAttribute(domElement, VDomDocument::AttrId, id);
+    doc->SetAttribute(domElement, AttrType, ToolType);
+    doc->SetAttribute(domElement, AttrIdObject, idNode);
+    if (idTool != NULL_ID)
+    {
+        doc->SetAttribute(domElement, AttrIdTool, idTool);
+    }
+
+    AddToModeling(domElement);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+ * @brief RefreshDataInFile refresh attributes in file. If attributes don't exist create them.
+ */
+void VNodeArc::RefreshDataInFile()
+{
+    QDomElement domElement = doc->elementById(id);
+    if (domElement.isElement())
+    {
+        doc->SetAttribute(domElement, AttrIdObject, idNode);
+        if (idTool != NULL_ID)
+        {
+            doc->SetAttribute(domElement, AttrIdTool, idTool);
+        }
+    }
+}
